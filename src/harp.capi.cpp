83c65369e674f5427a54760a862c6256f79e27cb
Guillaume Fraux <guillaume.fraux@ens.fr>
2014-12-28 01:35:14 +0100

    Silent warnings about no return type

--- src/harp.capi.cpp
+++ src/harp.capi.cpp
@@ -10,64 +10,62 @@
 #include "harp.h"
 #include "Harp.hpp"
 
-#include "logging.hpp"
-
 HARP_FRAME* harp_frame(const HARP_FILE*){
-
+    return NULL;
 }
 
 int harp_frame_size(const HARP_FRAME*){
-
+    return 0;
 }
 
 int harp_frame_positions(const HARP_FRAME*, float**, const int){
-
+    return 0;
 }
 
 int harp_frame_velocities(const HARP_FRAME*, float**, const int){
-
+    return 0;
 }
 
 HARP_ATOM* harp_get_atom(const HARP_FRAME*, const int){
-
+    return NULL;
 }
 
 int harp_frame_delete(HARP_FRAME*){
-
+    return 0;
 }
 
 /******************************************************************************/
 
 float harp_get_mass(const HARP_ATOM*){
-
+    return 0.0;
 }
 
 float harp_get_charge(const HARP_ATOM*){
-
+    return 0.0;
 }
 
 float harp_get_radius(const HARP_ATOM*){
-
+    return 0.0;
 }
 
 char* harp_get_name(const HARP_ATOM*, char*, const int){
-
+    return NULL;
 }
 
 /******************************************************************************/
 
 HARP_FILE* harp_open(const char*){
-
+    return NULL;
 }
 
 int harp_read_step(const HARP_FILE*, const int, HARP_FRAME*){
-
+    return 0;
 }
 
 int harp_read_next_step(const HARP_FILE*, HARP_FRAME*){
-
+    return 0;
 }
 
 int harp_close(HARP_FILE*){
-
+    return 0;
 }
