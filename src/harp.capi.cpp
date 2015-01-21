9f0a8b6cce85f7da7e4f95041ac5820d340c24cb
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-01-21 22:30:04 +0100

    C API implementation start

--- src/harp.capi.cpp
+++ src/harp.capi.cpp
@@ -7,65 +7,85 @@
 * file, You can obtain one at http://mozilla.org/MPL/2.0/
 */
 
+// Public headers
 #include "harp.h"
 #include "Harp.hpp"
 
-HARP_FRAME* harp_frame(const HARP_FILE*){
+// Private headers
+#include "Stream.hpp"
+#include "Frame.hpp"
+
+using namespace harp;
+
+HARP_FRAME* harp_frame(const HARP_FILE* file){
     return NULL;
 }
 
-int harp_frame_size(const HARP_FRAME*){
+int harp_frame_size(const HARP_FRAME* frame){
     return 0;
 }
 
-int harp_frame_positions(const HARP_FRAME*, float**, const int){
+int harp_frame_positions(const HARP_FRAME* frame, float *positions[], const int size){
     return 0;
 }
 
-int harp_frame_velocities(const HARP_FRAME*, float**, const int){
+int harp_frame_velocities(const HARP_FRAME* frame, float *velocities[], const int size){
     return 0;
 }
 
-HARP_ATOM* harp_get_atom(const HARP_FRAME*, const int){
+HARP_ATOM* harp_get_atom(const HARP_FRAME* frame, const int index){
     return NULL;
 }
 
-int harp_frame_delete(HARP_FRAME*){
+int harp_frame_delete(HARP_FRAME* frame){
     return 0;
 }
 
 /******************************************************************************/
 
-float harp_get_mass(const HARP_ATOM*){
+float harp_get_mass(const HARP_ATOM* atom){
     return 0.0;
 }
 
-float harp_get_charge(const HARP_ATOM*){
+float harp_get_charge(const HARP_ATOM* atom){
     return 0.0;
 }
 
-float harp_get_radius(const HARP_ATOM*){
+float harp_get_radius(const HARP_ATOM* atom){
     return 0.0;
 }
 
-char* harp_get_name(const HARP_ATOM*, char*, const int){
+char* harp_get_name(const HARP_ATOM* atom, char* name, const int size){
     return NULL;
 }
 
 /******************************************************************************/
 
-HARP_FILE* harp_open(const char*){
-    return NULL;
+HARP_FILE* harp_open(const char* filename){
+    HARP_FILE* file = NULL;
+    try {
+        file = new HarpFile(filename);
+    }
+    catch(harp::Error) {
+        // TODO
+    }
+    return file;
 }
 
-int harp_read_step(const HARP_FILE*, const int, HARP_FRAME*){
+int harp_read_step(const HARP_FILE* file, const int step, HARP_FRAME* frame){
     return 0;
 }
 
-int harp_read_next_step(const HARP_FILE*, HARP_FRAME*){
+int harp_read_next_step(const HARP_FILE* file, HARP_FRAME* frame){
     return 0;
 }
 
-int harp_close(HARP_FILE*){
+int harp_close(HARP_FILE* file){
+    try {
+        delete file;
+    }
+    catch(harp::Error) {
+        // TODO
+    }
     return 0;
 }
