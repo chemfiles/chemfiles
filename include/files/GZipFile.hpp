f04a5a85563ca19c1f558c6c31c96c94310764f8
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-01-21 22:16:17 +0100

    Reorganise the File(s) class hierarchy

---
+++ include/files/GZipFile.hpp
@@ -0,0 +1,22 @@
+/*
+* Harp, an efficient IO library for chemistry file formats
+* Copyright (C) 2014 Guillaume Fraux
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/
+*/
+
+#ifndef HARP_FILES_HPP
+#define HARP_FILES_HPP
+
+#include "files/File.hpp"
+
+namespace harp {
+
+// using the Boost.Iostreams lib
+// TODO: class GzipFile : public TextFile
+
+} // namespace harp
+
+#endif
