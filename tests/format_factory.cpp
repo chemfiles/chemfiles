38ba833bdec11edc2b0ab9baac6e276d6890eeac
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-02-20 23:40:03 +0100

    Add tests for registering the formats

--- tests/format_factory.cpp
+++ tests/format_factory.cpp
@@ -4,9 +4,10 @@
 
 #include "Chemharp.hpp"
 #include "FormatFactory.hpp"
-#include "Frame.hpp"
+#include "Error.hpp"
 #include "formats/XYZ.hpp"
 
+#include "Frame.hpp"
 using namespace harp;
 
 // Dummy format clase
@@ -19,7 +20,31 @@ public:
 REGISTER(DummyFormat, "Dummy");
 REGISTER_EXTENSION(DummyFormat, ".dummy");
 
-TEST_CASE("Get registered format", "[format factory]"){
+TEST_CASE("Registering a new format", "[format factory]"){
+    CHECK(FormatFactory::register_extension(".testing",[](){
+                return unique_ptr<Format>(new DummyFormat());
+         })
+    );
+    // We can not register the same format twice
+    CHECK_THROWS_AS(FormatFactory::register_extension(".testing",[](){
+                return unique_ptr<Format>(new DummyFormat());
+         }),
+         FormatError
+    );
+
+    CHECK(FormatFactory::register_format("DummyFormat",[](){
+                return unique_ptr<Format>(new DummyFormat());
+         })
+    );
+    // We can not register the same format twice
+    CHECK_THROWS_AS(FormatFactory::register_format("DummyFormat",[](){
+                return unique_ptr<Format>(new DummyFormat());
+         }),
+         FormatError
+    );
+}
+
+TEST_CASE("Geting registered format", "[format factory]"){
     auto dummy = DummyFormat();
     auto format = FormatFactory::by_extension(".dummy");
     REQUIRE(typeid(dummy) == typeid(*format));
