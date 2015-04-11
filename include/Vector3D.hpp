db7ddd44639a3bce8938de69b7c1ed2660394612
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-04-11 14:13:09 +0200

    Improve the Vector3D class

--- include/Vector3D.hpp
+++ include/Vector3D.hpp
@@ -12,12 +12,33 @@
 
 #include <array>
 #include <vector>
+#include <iostream>
 
 //! Fixed-size array of 3 components: x, y and z values.
-typedef std::array<float, 3> Vector3D;
+class Vector3D : public std::array<float, 3> {
+public:
+    Vector3D() : Vector3D(0, 0, 0) {}
+    Vector3D(float x, float y, float z){
+        (*this)[0] = x;
+        (*this)[1] = y;
+        (*this)[2] = z;
+    }
+
+    Vector3D(const Vector3D&) = default;
+    Vector3D(Vector3D&&) = default;
+
+    Vector3D& operator=(const Vector3D&) = default;
+    Vector3D& operator=(Vector3D&&) = default;
+
+    ~Vector3D() = default;
+};
+
+inline std::ostream& operator<<(std::ostream& out, const Vector3D& v){
+    out << v[0] << ", " << v[1] << ", " << v[2];
+    return out;
+}
+
 //! Variable-size array of vector of 3 components
 typedef std::vector<Vector3D> Array3D;
-
-#define Vector3D(x, y, z) (Vector3D{{(x), (y), (z)}})
 
 #endif
