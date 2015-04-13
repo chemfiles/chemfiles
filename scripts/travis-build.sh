bdee1bddd5afe80ff920a80af53da50c35df3bf6
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-04-13 22:58:13 +0200

    Use CMake to build netcdf_cxx
    The last version will not be available everywhere before a long time, so
    better build it.

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ev
 
-cmake -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE="release" .
-make
-ctest
+wget http://www.cmake.org/files/v3.2/cmake-3.2.1-Linux-x86_64.sh
+sh cmake-3.2.1-Linux-x86_64.sh --prefix=. --include-subdir
+
+./cmake-3.2.1-Linux-x86_64/bin/cmake -DBUILD_TESTS=ON .
+
+make -j2
+
+ctest --output-on-failure
