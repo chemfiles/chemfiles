848573a3b81502a2da2fff8e63679c4cc8942683
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-04-15 23:16:46 +0200

    Add all missing functions to the Python Binding

--- bindings/python/chemharp.cpp
+++ bindings/python/chemharp.cpp
@@ -6,24 +6,25 @@
 namespace py = boost::python;
 namespace np = boost::numpy;
 using namespace harp;
+using std::string;
 
 void translate_Error(Error const& e) {
-    auto err = std::string("Chemharp error: ") + e.what();
+    auto err = string("Chemharp error: ") + e.what();
     PyErr_SetString(PyExc_UserWarning, err.c_str());
 }
 
 void translate_FileError(FileError const& e) {
-    auto err = std::string("Chemharp file error: ") + e.what();
+    auto err = string("Chemharp file error: ") + e.what();
     PyErr_SetString(PyExc_UserWarning, err.c_str());
 }
 
 void translate_MemoryError(MemoryError const& e) {
-    auto err = std::string("Chemharp memory error: ") + e.what();
+    auto err = string("Chemharp memory error: ") + e.what();
     PyErr_SetString(PyExc_UserWarning, err.c_str());
 }
 
 void translate_FormatError(FormatError const& e) {
-    auto err = std::string("Chemharp format error: ") + e.what();
+    auto err = string("Chemharp format error: ") + e.what();
     PyErr_SetString(PyExc_UserWarning, err.c_str());
 }
 
@@ -39,6 +40,30 @@ struct Array3D_convertor {
 
                 auto c_arr = reinterpret_cast<float (*)[3]>(res.get_data());
                 for (size_t i=0; i<A.size(); i++)
+                    for (size_t j=0; j<3; j++)
+                        c_arr[i][j] = A[i][j];
+
+                return py::incref(res.ptr());
+            }
+
+            // Used for documentation.
+            const PyTypeObject* get_pytype() const { return 0; }
+        };
+    };
+};
+
+// ResultConverterGenerator used to transform Matrix3D to numpy ndarray.
+struct Matrix3D_convertor {
+    template <class T> struct apply {
+        struct type {
+            // Convert Matrix3D to ndarray.
+            PyObject* operator()(const Matrix3D& A) const {
+                py::tuple shape = py::make_tuple(3, 3);
+                np::dtype dtype = np::dtype::get_builtin<double>();
+                np::ndarray res = np::empty(shape, dtype);
+
+                auto c_arr = reinterpret_cast<double (*)[3]>(res.get_data());
+                for (size_t i=0; i<3; i++)
                     for (size_t j=0; j<3; j++)
                         c_arr[i][j] = A[i][j];
 
@@ -69,6 +94,10 @@ struct std_vector_convertor {
     };
 };
 
+void topology_setitem(Topology& top, size_t index, const Atom& atom) {
+    top[index] = atom;
+}
+
 void init_module(){
     // Removing this line will result in bad stuff appening, like segfaults and
     // your grand mother being kidnaped by aliens. So don't do this!
@@ -87,14 +116,17 @@ BOOST_PYTHON_MODULE(chemharp){
 
     /* Trajectory class *******************************************************/
     py::class_<Trajectory, boost::noncopyable>("Trajectory",
-            py::init<std::string, py::optional<std::string, std::string>>())
-        .def("read_next_step", &Trajectory::read_next_step,
-                                    py::return_internal_reference<1,
-                                        py::with_custodian_and_ward_postcall<0, 1> >())
-        .def("read_at_step", &Trajectory::read_at_step,
-                                    py::return_internal_reference<1,
-                                        py::with_custodian_and_ward_postcall<0, 1> >())
+            py::init<string, py::optional<string, string>>())
+        .def("read_next_step", &Trajectory::read_next_step)
+        .def("read_at_step", &Trajectory::read_at_step)
         .def("write_step", &Trajectory::write_step)
+        .def("done", &Trajectory::done)
+        .def("close", &Trajectory::close)
+        .def("nsteps", &Trajectory::nsteps)
+        .def("topology",
+            static_cast<void (Trajectory::*)(const Topology&)>(&Trajectory::topology))
+        .def("topology",
+            static_cast<void (Trajectory::*)(const string&)>(&Trajectory::topology))
     ;
 
     /* Frame class ************************************************************/
@@ -124,12 +156,12 @@ BOOST_PYTHON_MODULE(chemharp){
     ;
 
     /* Atom class *************************************************************/
-    py::scope atom_scope = py::class_<Atom>("Atom", py::init<std::string>())
+    py::scope atom_scope = py::class_<Atom>("Atom", py::init<string>())
         .add_property("name",
             py::make_function(
-                static_cast<const std::string& (Atom::*)(void) const>(&Atom::name),
+                static_cast<const string& (Atom::*)(void) const>(&Atom::name),
                 py::return_value_policy<py::copy_const_reference>()),
-            static_cast<void (Atom::*)(const std::string&)>(&Atom::name))
+            static_cast<void (Atom::*)(const string&)>(&Atom::name))
         .add_property("mass",
             py::make_function(
                 static_cast<const float& (Atom::*)(void) const>(&Atom::mass),
@@ -145,6 +177,10 @@ BOOST_PYTHON_MODULE(chemharp){
                 static_cast<const Atom::AtomType& (Atom::*)(void) const>(&Atom::type),
                 py::return_value_policy<py::copy_const_reference>()),
             static_cast<void (Atom::*)(Atom::AtomType)>(&Atom::type))
+        .def("full_name", &Atom::full_name)
+        .def("vdw_radius", &Atom::vdw_radius)
+        .def("covalent_radius", &Atom::covalent_radius)
+        .def("atomic_number", &Atom::atomic_number)
     ;
 
     /* AtomType enum **********************************************************/
@@ -158,24 +194,36 @@ BOOST_PYTHON_MODULE(chemharp){
     /* Topology class *********************************************************/
     py::class_<Topology>("Topology")
         .def("append", &Topology::append)
+        .def("remove", &Topology::remove)
         .def("add_bond", &Topology::add_bond)
+        .def("remove_bond", &Topology::remove_bond)
+
         .def("__len__", &Topology::natoms)
         .add_property("natoms", &Topology::natoms)
         .add_property("natom_types", &Topology::natom_types)
+
         .def("clear", &Topology::clear)
         .def("resize", &Topology::resize)
-/*      TODO:
-        operator[]
-        void guess_bonds();
+
+        .def("isbond", &Topology::isbond)
+        .def("isangle", &Topology::isangle)
+        .def("isdihedral", &Topology::isdihedral)
+
+        .def("guess_bonds", &Topology::guess_bonds)
+
+        .def( "__getitem__",
+                static_cast<const Atom& (Topology::*)(size_t) const>(&Topology::operator[]),
+                py::return_internal_reference<>())
+        .def( "__setitem__", &topology_setitem)
+
+        /* TODO:
         vector<bond> bonds(void);
         vector<angle> angles(void);
         vector<dihedral> dihedrals(void);
-        isbond,
-        isangle,
-        isdihedral,
-        constuctor
-*/
+        */
     ;
+
+    py::def("dummy_topology", dummy_topology);
 
     /* UnitCell class *********************************************************/
     py::class_<UnitCell>("UnitCell", py::init<>())
@@ -185,11 +233,9 @@ BOOST_PYTHON_MODULE(chemharp){
         .def(py::init<UnitCell::CellType>())
         .def(py::init<UnitCell::CellType, double>())
         .def(py::init<UnitCell::CellType, double, double, double>())
-        // TODO Matrix3D matricial() const;
+        .def("matricial", &UnitCell::matricial, py::return_value_policy<Matrix3D_convertor>())
         .add_property("type",
-            py::make_function(
-                static_cast<const UnitCell::CellType& (UnitCell::*)(void) const>(&UnitCell::type),
-                py::return_value_policy<py::copy_const_reference>()),
+            static_cast<UnitCell::CellType (UnitCell::*)(void) const>(&UnitCell::type),
             static_cast<void (UnitCell::*)(UnitCell::CellType)>(&UnitCell::type))
         .add_property("a",
             static_cast<double (UnitCell::*)(void) const>(&UnitCell::a),
@@ -230,6 +276,23 @@ BOOST_PYTHON_MODULE(chemharp){
         .value("INFINITE", UnitCell::INFINITE)
     ;
 
-    // TODO: Logger class
+    /* LogLevel enum **********************************************************/
+    py::enum_<Logger::LogLevel>("LogLevel")
+        .value("NONE", Logger::NONE)
+        .value("ERROR", Logger::ERROR)
+        .value("WARNING", Logger::WARNING)
+        .value("INFO", Logger::INFO)
+        .value("DEBUG", Logger::DEBUG)
+    ;
 
+    /* Logger class ***********************************************************/
+    py::class_<Logger, boost::noncopyable>("Logger", py::no_init)
+        .add_property("level",
+            static_cast<Logger::LogLevel (*)(void)>(&Logger::level),
+            static_cast<void (*)(Logger::LogLevel)>(&Logger::level))
+        .def("log_to_file", &Logger::log_to_file)
+            .staticmethod("log_to_file")
+        .def("log_to_stdout", &Logger::log_to_stdout)
+            .staticmethod("log_to_stdout")
+    ;
 }
