54719778e642346c45fc7b224861b00523ded4b6
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-04-30 16:27:01 +0200

    Enable Fortran testing on Travis

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -4,8 +4,10 @@ set -ev
 wget http://www.cmake.org/files/v3.2/cmake-3.2.1-Linux-x86_64.sh
 sh cmake-3.2.1-Linux-x86_64.sh --prefix=. --include-subdir
 
-./cmake-3.2.1-Linux-x86_64/bin/cmake -DBUILD_TESTS=ON -DPYTHON_BINDING=ON .
+./cmake-3.2.1-Linux-x86_64/bin/cmake -DBUILD_TESTS=ON \
+                                     -DPYTHON_BINDING=ON \
+                                     -DFORTRAN_BINDING=ON . .
 
-make -j2
+make
 
 ctest --output-on-failure
