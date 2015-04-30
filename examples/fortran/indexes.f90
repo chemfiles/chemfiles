b489c89e872d0e613942f721e1fa6935139d17e1
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-04-30 16:23:12 +0200

    Get the fortran example to compile

--- examples/fortran/indexes.f90
+++ examples/fortran/indexes.f90
@@ -1,27 +1,28 @@
 program indexes_example
-    use iso_fortran_env  ! For real64
+    use iso_fortran_env, only: real32, int64
     use chemharp
     implicit none
     ! Chemharp types declaration uses the "chrp_" prefix
     type(chrp_trajectory) :: traj
     type(chrp_frame) :: frame
-    type(chrp_frame) :: frame
 
-    real(real64), dimension(:, :), allocatable :: positions
+    real(real32), dimension(:, :), allocatable :: positions
     integer, dimension(:), allocatable :: indexes
-    integer :: status, i, j, natoms
+    integer :: i, j, status
+    integer(int64) :: natoms
 
-    call traj%init("filename.xyz", status)
-    if (status /= 0) then stop end if
+    call traj%open("filename.xyz", "r")
 
-    call traj%read_next_step(frame, status=status)
-    if (status /= 0) then stop end if
+    call traj%read(frame, status=status)
+    if (status /= 0) then
+        stop "Error"
+    end if
 
     ! Read the number of atoms
-    call frame%natoms(natoms)
+    call frame%size(natoms)
 
     allocate(indexes(natoms), positions(3, natoms))
-    call frame%positions(positions)
+    call frame%positions(positions, natoms)
 
     indexes = -1
     j = 1
@@ -35,7 +36,7 @@ program indexes_example
     write(*,*) "Atoms with x < 5: "
     do i=1,natoms
         if (indexes(i) == -1) then
-            break
+            exit
         end if
         write(*,*) "  - ", indexes(i)
     end do
