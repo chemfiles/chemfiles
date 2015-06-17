f37b5eacdbbce2b56570ab09d5dba61e8f91d498
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-06-17 15:39:36 +0200

    More systematic testing for the fortran interface

--- bindings/c/tests/chrp_trajectory_read.c
+++ bindings/c/tests/chrp_trajectory_read.c
@@ -13,6 +13,7 @@ int main(){
     CHRP_FRAME* frame = chrp_frame(0);
     CHRP_TRAJECTORY* file = chrp_open(DATADIR "/xyz/water.xyz", "r");
 
+    // Read the first frame
     assert(!chrp_trajectory_read(file, frame));
 
     size_t natoms=0;
@@ -23,16 +24,20 @@ int main(){
     float pos_124[3] = {5.099554, -0.045104, 14.153846};
     float pos[297][3];
 
+    // Check positions in the first frame
     assert(!chrp_frame_positions(frame, pos, 297));
     for (unsigned i=0; i<3; i++){
         assert(pos[0][i] == pos_0[i]);
         assert(pos[124][i] == pos_124[i]);
     }
 
+    // Check topology in the first frame
     CHRP_TOPOLOGY* topology = chrp_topology_from_frame(frame);
     assert(!chrp_topology_size(topology, &natoms));
     assert(natoms == 297);
-
+    size_t n = 0;
+    assert(!chrp_topology_bonds_count(topology, &n));
+    assert(n == 0);
 
     CHRP_ATOM* atom = chrp_atom_from_topology(topology, 0);
     char name[32];
@@ -41,11 +46,15 @@ int main(){
     assert(!chrp_atom_free(atom));
     assert(!chrp_topology_free(topology));
 
+    // Set the cell associated with a trajectory
     CHRP_CELL* cell = chrp_cell(30, 30, 30);
     assert(!chrp_trajectory_set_cell(file, cell));
     assert(!chrp_cell_free(cell));
 
+    // Check reading a specific step
     assert(!chrp_trajectory_read_step(file, 41, frame));
+
+    // Check that the cell was set
     cell = chrp_cell_from_frame(frame);
     double a=0, b=0, c=0;
     assert(!chrp_cell_lengths(cell, &a, &b, &c));
@@ -54,8 +63,7 @@ int main(){
     assert(c == 30.0);
     assert(!chrp_cell_free(cell));
 
-    assert(!chrp_trajectory_read_step(file, 41, frame));
-    pos_0[0] = 0.761277; pos_0[1] = 8.106125; pos_0[2] = 10.622949;
+    pos_0[0] = 0.761277;  pos_0[1] = 8.106125;   pos_0[2] = 10.622949;
     pos_124[0] = 5.13242; pos_124[1] = 0.079862; pos_124[2] = 14.194161;
 
     assert(!chrp_frame_positions(frame, pos, 297));
@@ -64,25 +72,14 @@ int main(){
         assert(pos[124][i] == pos_124[i]);
     }
 
-    topology = chrp_topology_from_frame(frame);
-    assert(!chrp_topology_size(topology, &natoms));
-    assert(natoms == 297);
 
-    size_t n=10;
-    assert(!chrp_topology_bonds_count(topology, &n));
-    assert(n == 0);
-    assert(!chrp_topology_free(topology));
-
-    atom = chrp_atom_from_frame(frame, 0);
-    assert(!chrp_atom_name(atom, name, sizeof(name)));
-    assert(strcmp(name, "O") == 0);
-    assert(!chrp_atom_free(atom));
+    // Get the atom from a frame
     atom = chrp_atom_from_frame(frame, 1);
     assert(!chrp_atom_name(atom, name, sizeof(name)));
     assert(strcmp(name, "H") == 0);
     assert(!chrp_atom_free(atom));
 
-
+    // Guess the system topology
     assert(!chrp_frame_guess_topology(frame, true));
     topology = chrp_topology_from_frame(frame);
     assert(!chrp_topology_bonds_count(topology, &n));
@@ -91,13 +88,15 @@ int main(){
     assert(n == 87);
     assert(!chrp_topology_free(topology));
 
-
+    // Set the topology associated with a trajectory by hand
     topology = chrp_topology();
     atom = chrp_atom("Cs");
     for (unsigned i=0; i<3; i++)
         assert(!chrp_topology_append(topology, atom));
 
     assert(!chrp_trajectory_set_topology(file, topology));
+    assert(!chrp_topology_free(topology));
+    
     assert(!chrp_trajectory_read_step(file, 10, frame));
 
     atom = chrp_atom_from_frame(frame, 1);
@@ -105,6 +104,7 @@ int main(){
     assert(strcmp(name, "Cs") == 0);
     assert(!chrp_atom_free(atom));
 
+    // Set the topology associated with a trajectory from a file
     assert(!chrp_trajectory_set_topology_file(file, DATADIR "/xyz/topology.xyz"));
     assert(!chrp_trajectory_read(file, frame));
     atom = chrp_atom_from_frame(frame, 0);
