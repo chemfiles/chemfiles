0792897c7bc0361e349d151fc65543a8fd9fb3b7
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-10-07 21:58:30 +0200

    Remove conda recipe. Its belong to the Chemharp.py repository

--- scripts/conda-recipe/README.md
+++
@@ -1,3 +0,0 @@
-# Conda recipe for Chemharp
-
-This is a [conda](http://conda.pydata.org/) recipe for packaging Chemharp for Python.
--- scripts/conda-recipe/bld.bat
+++
@@ -1,6 +0,0 @@
-cd %SRC_DIR%
-mkdir build
-cd build
-
-cmake .. -DBUILD_TESTS=ON -DPYTHON_BINDING=ON -DCMAKE_INSTALL_PREFIX=%PREFIX%
-cmake --build . --target install
