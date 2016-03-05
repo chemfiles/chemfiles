b3a6e0da37366dfdc412e75877e26cfdb12d6ba9
Luthaf <luthaf@luthaf.fr>
2016-03-06 00:10:47 +0100

    Remove unused linked library
    Libdl was used for the shared library loading with molfiles plugins

--- cmake/Platforms.cmake
+++
@@ -1,9 +0,0 @@
-# Plateform specific configuration
-
-set(OTHER_CHEMFILES_LIBRARIES "")
-
-if(${UNIX})
-    find_library(LIBDL_LIBRARY NAMES dl)
-    set(OTHER_CHEMFILES_LIBRARIES ${LIBDL_LIBRARY})
-endif()
-mark_as_advanced(LIBDL_LIBRARY)
