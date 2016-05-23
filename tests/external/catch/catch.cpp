388291474e620c64c139806dca5cbd75489f2b39
Guillaume Fraux <guillaume.fraux@ens.fr>
2016-05-23 10:42:13 +0200

    Fix cross-compilation with mingw32

--- tests/external/catch/catch.cpp
+++ tests/external/catch/catch.cpp
@@ -5,18 +5,16 @@
 // requires an human intervention, and there is no one on Appveyor.
 //
 // On UNIX, does nothing
-void silent_crash_handlers();
+#if (defined(WIN32) || defined(WIN64))
+    #include <windows.h>
+    void silent_crash_handlers() {
+        SetErrorMode(0 | SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
+    }
+#else
+    void silent_crash_handlers() {}
+#endif
 
 int main(int argc, char* argv[]) {
     silent_crash_handlers();
     return Catch::Session().run(argc, argv);
 }
-
-#if (defined(WIN32) || defined(WIN64))
-    #include <windows.h>
-    void silent_crash_handlers() {
-        SetErrorMode(GetErrorMode() | SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
-    }
-#else
-    void silent_crash_handlers() {}
-#endif
