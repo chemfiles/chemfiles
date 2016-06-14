96d75222b39980a5b61c8603417403c3c4a62bf8
Guillaume Fraux <guillaume.fraux@ens.fr>
2016-06-14 15:01:01 +0200

    More tests for Frame

--- tests/frame.cpp
+++ tests/frame.cpp
@@ -1,36 +1,44 @@
 #include <catch.hpp>
 #include "chemfiles.hpp"
-#include "chemfiles/Frame.hpp"
-#include "chemfiles/UnitCell.hpp"
 using namespace chemfiles;
 
 TEST_CASE("Frame class usage", "[Frame]"){
-    SECTION("Contructor"){
+    SECTION("Size"){
         auto frame = Frame(10);
+        CHECK(frame.natoms() == 10);
         CHECK(frame.positions().size() == 10);
-        CHECK(frame.cell().type() == UnitCell::INFINITE);
-    }
-
-    SECTION("Get and set"){
-        auto frame = Frame();
-        CHECK(frame.step() == 0);
-        frame.set_step(1000);
-        CHECK(frame.step() == 1000);
-
-        frame.set_cell(UnitCell(10));
-        CHECK(frame.cell().type() == UnitCell::ORTHORHOMBIC);
-
-        frame.resize(15);
-        CHECK(frame.natoms() == 15);
         // No velocity data yet
         CHECK_FALSE(frame.velocities());
 
+        frame.resize(15);
+        CHECK(frame.natoms() == 15);
+        CHECK(frame.positions().size() == 15);
+        CHECK_FALSE(frame.velocities());
+
         frame.add_velocities();
-        CHECK(frame.velocities());
+        REQUIRE(frame.velocities());
+        CHECK(frame.velocities()->size() == 15);
+
+        frame.resize(30);
+        CHECK(frame.natoms() == 30);
+        CHECK(frame.positions().size() == 30);
+        REQUIRE(frame.velocities());
+        CHECK(frame.velocities()->size() == 30);
+
+        frame.resize(2);
+        CHECK(frame.natoms() == 2);
+        CHECK(frame.positions().size() == 2);
+        REQUIRE(frame.velocities());
+        CHECK(frame.velocities()->size() == 2);
+    }
+
+    SECTION("Positions and velocities"){
+        auto frame = Frame(15);
 
         frame.positions()[0] = vector3d(1, 2, 3);
         CHECK(frame.positions()[0] == vector3d(1, 2, 3));
 
+        frame.add_velocities();
         (*frame.velocities())[0] = vector3d(5, 6, 7);
         CHECK((*frame.velocities())[0] == vector3d(5, 6, 7));
 
@@ -51,10 +59,22 @@ TEST_CASE("Frame class usage", "[Frame]"){
         }
     }
 
-    SECTION("Guess bonds"){
-        Trajectory file(SRCDIR "/data/xyz/methane.xyz");
+    SECTION("Step"){
+        auto frame = Frame();
+        CHECK(frame.step() == 0);
+        frame.set_step(1000);
+        CHECK(frame.step() == 1000);
+    }
 
-        auto frame = file.read();
+    SECTION("Unit Cell"){
+        auto frame = Frame();
+        CHECK(frame.cell().type() == UnitCell::INFINITE);
+        frame.set_cell(UnitCell(10));
+        CHECK(frame.cell().type() == UnitCell::ORTHORHOMBIC);
+    }
+
+    SECTION("Topology"){
+        auto frame = Trajectory(SRCDIR "/data/xyz/methane.xyz").read();
         frame.guess_topology();
 
         auto topology = frame.topology();
@@ -70,5 +90,9 @@ TEST_CASE("Frame class usage", "[Frame]"){
         CHECK(topology.isangle(2, 0, 4));
 
         CHECK(topology.bonds().size() == 4);
+
+        // Wrong topology size
+        frame = Frame(5);
+        CHECK_THROWS_AS(frame.set_topology(Topology()), APIError);
     }
 }
