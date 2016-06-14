42013d5ba1d46ff5d61755de288355062d281d5c
Guillaume Fraux <guillaume.fraux@ens.fr>
2016-06-14 14:19:01 +0200

    Add a test for multiple selections usage from C

--- tests/c/chfl_selections.c
+++ tests/c/chfl_selections.c
@@ -23,6 +23,7 @@ int main() {
     assert(n_matches == 2);
 
     chfl_match_t *matches = malloc(n_matches * sizeof(chfl_match_t));
+    assert(matches);
     assert(!chfl_selection_matches(selection, matches, n_matches));
     assert(matches[0].size == 1);
     assert(matches[1].size == 1);
@@ -45,11 +46,38 @@ int main() {
     assert(n_matches == 1);
 
     matches = malloc(n_matches * sizeof(chfl_match_t));
+    assert(matches);
     assert(!chfl_selection_matches(selection, matches, n_matches));
     assert(matches[0].atoms[0] == 3);
 
     free(matches);
     assert(!chfl_selection_free(selection));
+
+    selection = NULL;
+    selection = chfl_selection("angles: all");
+    assert(selection != NULL);
+
+    assert(!chfl_selection_size(selection, &size));
+    assert(size == 3);
+
+    assert(!chfl_selection_evalutate(selection, frame, &n_matches));
+    assert(n_matches == 2);
+
+    matches = malloc(n_matches * sizeof(chfl_match_t));
+    assert(matches);
+    assert(!chfl_selection_matches(selection, matches, n_matches));
+    assert(matches[0].size == 3);
+    assert(matches[1].size == 3);
+    assert(matches[0].atoms[0] == 0);
+    assert(matches[0].atoms[1] == 1);
+    assert(matches[0].atoms[2] == 2);
+    assert(matches[1].atoms[0] == 1);
+    assert(matches[1].atoms[1] == 2);
+    assert(matches[1].atoms[2] == 3);
+
+    free(matches);
+    assert(!chfl_selection_free(selection));
+
     assert(!chfl_frame_free(frame));
     return EXIT_SUCCESS;
 }
@@ -68,6 +96,10 @@ CHFL_FRAME* testing_frame() {
     assert(!chfl_topology_append(topology, H));
     assert(!chfl_atom_free(O));
     assert(!chfl_atom_free(H));
+
+    assert(!chfl_topology_add_bond(topology, 0, 1));
+    assert(!chfl_topology_add_bond(topology, 1, 2));
+    assert(!chfl_topology_add_bond(topology, 2, 3));
 
     CHFL_FRAME* frame = chfl_frame(4);
     assert(frame != NULL);
