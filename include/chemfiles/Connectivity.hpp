048e01a4076e550887df97e73a4eb4f5f364602f
Guillaume Fraux <guillaume.fraux@ens.fr>
2016-09-02 17:35:21 +0200

    Separate the Connectivity and Topology classes

---
+++ include/chemfiles/Connectivity.hpp
@@ -0,0 +1,163 @@
+/* Chemfiles, an efficient IO library for chemistry file formats
+ * Copyright (C) 2015 Guillaume Fraux
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/
+*/
+
+#ifndef CHEMFILES_CONNECTIVITY_HPP
+#define CHEMFILES_CONNECTIVITY_HPP
+
+#include <array>
+#include <cassert>
+#include <unordered_set>
+
+#include "chemfiles/exports.hpp"
+
+namespace chemfiles {
+
+//! The bond struct ensure a canonical representation of a bond between atoms
+//! i and j, with i<j
+struct CHFL_EXPORT Bond {
+    Bond(size_t first, size_t second) {
+        assert(first != second);
+        data_[0] = std::min(first, second);
+        data_[1] = std::max(first, second);
+    }
+    //! Indexing operator
+    const size_t& operator[](size_t i) const { return data_[i]; }
+    //! Comparison operator
+    bool operator==(const Bond& other) const {
+        return data_[0] == other[0] && data_[1] == other[1];
+    }
+
+    Bond(Bond&&) = default;
+    Bond& operator=(Bond&&) = default;
+    Bond(const Bond&) = default;
+    Bond& operator=(const Bond&) = default;
+
+private:
+    std::array<size_t, 2> data_;
+};
+
+//! The angle struct ensure a canonical representation of an angle between the
+//! atoms i, j and k, with i < k
+struct CHFL_EXPORT Angle {
+    Angle(size_t first, size_t midle, size_t last) {
+        assert(first != last);
+        assert(first != midle);
+        data_[0] = std::min(first, last);
+        data_[1] = midle;
+        data_[2] = std::max(first, last);
+    }
+    //! Indexing operator
+    const size_t& operator[](size_t i) const { return data_[i]; }
+    //! Comparison operator
+    bool operator==(const Angle& other) const {
+        return data_[0] == other[0] && data_[1] == other[1] &&
+               data_[2] == other[2];
+    }
+
+    Angle(Angle&&) = default;
+    Angle& operator=(Angle&&) = default;
+    Angle(const Angle&) = default;
+    Angle& operator=(const Angle&) = default;
+
+private:
+    std::array<size_t, 3> data_;
+};
+
+//! The dihedral struct ensure a canonical representation of a dihedral angle
+//! between the atoms i, j, k and m, with max(i, j) < max(k, m))
+struct CHFL_EXPORT Dihedral {
+    Dihedral(size_t first, size_t second, size_t third, size_t fourth) {
+        assert(first != second);
+        assert(second != third);
+        assert(third != fourth);
+
+        if (std::max(first, second) < std::max(third, fourth)) {
+            data_[0] = first;
+            data_[1] = second;
+            data_[2] = third;
+            data_[3] = fourth;
+        } else {
+            data_[0] = fourth;
+            data_[1] = third;
+            data_[2] = second;
+            data_[3] = first;
+        }
+    }
+    //! Indexing operator
+    const size_t& operator[](size_t i) const { return data_[i]; }
+    //! Comparison operator
+    bool operator==(const Dihedral& other) const {
+        return data_[0] == other[0] && data_[1] == other[1] &&
+               data_[2] == other[2] && data_[3] == other[3];
+    }
+
+    Dihedral(Dihedral&&) = default;
+    Dihedral& operator=(Dihedral&&) = default;
+    Dihedral(const Dihedral&) = default;
+    Dihedral& operator=(const Dihedral&) = default;
+
+private:
+    std::array<size_t, 4> data_;
+};
+
+} // namespace chemfiles
+
+namespace std {
+    // We need a hashing function for the std::unordered_set, but it will not
+    // be used. We will use the operator== instead to ensure the element
+    // unicity in the sets.
+    template <> struct hash<chemfiles::Bond> {
+        size_t operator()(chemfiles::Bond const&) const { return 42; }
+    };
+    template <> struct hash<chemfiles::Angle> {
+        size_t operator()(chemfiles::Angle const&) const { return 42; }
+    };
+    template <> struct hash<chemfiles::Dihedral> {
+        size_t operator()(chemfiles::Dihedral const&) const { return 42; }
+    };
+} // namespace std
+
+namespace chemfiles {
+
+/*!
+ * @class connectivity Topology.hpp Topology.cpp
+ *
+ * The connectivity struct store a cache of the bonds, angles and dihedrals
+ * in the system. The `recalculate` function should be called when bonds are
+ * added or removed. The `bonds` set is the main source of information, all the
+ * other data are cached from it.
+ */
+class Connectivity {
+public:
+    Connectivity() = default;
+    //! Recalculate the angles and the dihedrals from the bond list
+    void recalculate() const;
+    //! Access the underlying data
+    const std::unordered_set<Bond>& bonds() const;
+    const std::unordered_set<Angle>& angles() const;
+    const std::unordered_set<Dihedral>& dihedrals() const;
+    //! Add a bond between the atoms `i` and `j`
+    void add_bond(size_t i, size_t j);
+    //! Remove any bond between the atoms `i` and `j`
+    void remove_bond(size_t i, size_t j);
+
+private:
+    //! Bonds in the system
+    std::unordered_set<Bond> bonds_;
+    //! Angles in the system
+    mutable std::unordered_set<Angle> angles_;
+    //! Dihedral angles in the system
+    mutable std::unordered_set<Dihedral> dihedrals_;
+    //! Is the cached content up to date ?
+    mutable bool uptodate = false;
+};
+
+
+} // namespace chemfiles
+
+#endif
---
+++ src/Connectivity.cpp
@@ -0,0 +1,94 @@
+/* Chemfiles, an efficient IO library for chemistry file formats
+ * Copyright (C) 2015 Guillaume Fraux
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/
+*/
+
+#include "chemfiles/Connectivity.hpp"
+
+using namespace chemfiles;
+
+void Connectivity::recalculate() const {
+    angles_.clear();
+    dihedrals_.clear();
+    for (auto const& bond1 : bonds_) {
+        // Find angles
+        for (auto const& bond2 : bonds_) {
+            if (bond1 == bond2) {
+                continue;
+            }
+            // Initializing angle to an invalid value
+            Angle angle1(static_cast<size_t>(-1), static_cast<size_t>(-2),
+                         static_cast<size_t>(-3));
+            if (bond1[0] == bond2[1]) {
+                angle1 = Angle(bond2[0], bond2[1], bond1[1]);
+                angles_.insert(angle1);
+            } else if (bond1[1] == bond2[0]) {
+                angle1 = Angle(bond1[0], bond1[1], bond2[1]);
+                angles_.insert(angle1);
+            } else if (bond1[1] == bond2[1]) {
+                angle1 = Angle(bond1[0], bond1[1], bond2[0]);
+                angles_.insert(angle1);
+            } else if (bond1[0] == bond2[0]) {
+                angle1 = Angle(bond1[1], bond1[0], bond2[1]);
+                angles_.insert(angle1);
+            } else {
+                // We will not find any dihedral angle from these bonds
+                continue;
+            }
+            // Find dihedral angles
+            for (auto const& bond3 : bonds_) {
+                if (bond2 == bond3) {
+                    continue;
+                }
+
+                if (angle1[2] == bond3[0] && angle1[1] != bond3[1]) {
+                    dihedrals_.emplace(angle1[0], angle1[1], angle1[2],
+                                       bond3[1]);
+                } else if (angle1[0] == bond3[1] && angle1[1] != bond3[0]) {
+                    dihedrals_.emplace(bond3[0], angle1[0], angle1[1],
+                                       angle1[2]);
+                } else if (angle1[2] == bond3[0] || angle1[2] == bond3[1]) {
+                    // TODO this is an improper dihedral
+                }
+            }
+        }
+    }
+    uptodate = true;
+}
+
+const std::unordered_set<Bond>& Connectivity::bonds() const {
+    if (!uptodate) {
+        recalculate();
+    }
+    return bonds_;
+}
+
+const std::unordered_set<Angle>& Connectivity::angles() const {
+    if (!uptodate) {
+        recalculate();
+    }
+    return angles_;
+}
+
+const std::unordered_set<Dihedral>& Connectivity::dihedrals() const {
+    if (!uptodate) {
+        recalculate();
+    }
+    return dihedrals_;
+}
+
+void Connectivity::add_bond(size_t i, size_t j) {
+    uptodate = false;
+    bonds_.emplace(i, j);
+}
+
+void Connectivity::remove_bond(size_t i, size_t j) {
+    auto pos = bonds_.find(Bond(i, j));
+    if (pos != bonds_.end()) {
+        uptodate = false;
+        bonds_.erase(pos);
+    }
+}
