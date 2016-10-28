680ab65372cd71c632cacc8596e388b1b5fa03f9
Guillaume Fraux <guillaume.fraux@ens.fr>
2016-10-28 16:16:35 +0200

    Only call _set_abort_behavior with MSVC

--- tests/external/helpers/helpers.cpp
+++ tests/external/helpers/helpers.cpp
@@ -11,7 +11,9 @@
 extern "C" void silent_crash_handlers() {
 #if defined(WIN32) || defined(WIN64)
     SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
+    #ifdef _MSC_VER
     _set_abort_behavior(0, _WRITE_ABORT_MSG);
+    #endif
 #endif
 }
 
