1532aa422aec619391aa7219593284ea3d599266
Guillaume Fraux <guillaume.fraux@ens.fr>
2016-12-05 16:08:27 +0100

    Set the atomic occupancy to 1 in PDB

--- src/formats/PDB.cpp
+++ src/formats/PDB.cpp
@@ -336,7 +336,7 @@ void PDBFormat::write(const Frame& frame) {
         fmt::print(
             *file_,
             "HETATM{: >5} {: >4s} {:3} X{: >4s}    {:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}          {: >2s}\n",
-            to_pdb_index(i), name, resname, resid, pos[0], pos[1], pos[2], 0.0, 0.0, type
+            to_pdb_index(i), name, resname, resid, pos[0], pos[1], pos[2], 1.0, 0.0, type
         );
     }
 
