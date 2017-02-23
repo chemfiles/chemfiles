e77f9e49c478bb2f85caa38398348fcaf902b4d8
pelsa <elsa.perrin@ens.fr>
2017-02-23 16:48:02 +0100

    fix recursive import in doc
    commit done with @Luthaf

--- doc/conf.cmake.py
+++ doc/conf.cmake.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 sys.path.append("@CMAKE_CURRENT_SOURCE_DIR@")
-from conf import *
+from global_conf import *
 
 DOC_ROOT = os.path.abspath(os.path.dirname(__file__))
 breathe_projects = {"chemfiles": os.path.join(DOC_ROOT, "doxygen", "xml")}
