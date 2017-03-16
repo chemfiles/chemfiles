30578b2baeb4071b9984a8f94be1085fec84ca35
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-03-16 16:31:32 +0100

    Handle all possible line ending markers

--- src/files/BasicFile.cpp
+++ src/files/BasicFile.cpp
@@ -11,6 +11,42 @@
 #include "chemfiles/Error.hpp"
 using namespace chemfiles;
 
+/// Read a single line from `stream` in `string`, handling all possible end of
+/// line markers (`\n`, `\r`, `\r\n`).
+///
+/// Code from http://stackoverflow.com/a/6089413
+static inline std::istream& get_line(std::istream& istream, std::string& string) {
+    string.clear();
+
+    // The characters in the stream are read one-by-one using a std::streambuf.
+    // That is faster than reading them one-by-one using the std::istream.
+    // Code that uses streambuf this way must be guarded by a sentry object.
+    // The sentry object performs various tasks,
+    // such as thread synchronization and updating the stream state.
+    std::istream::sentry sentry(istream, true);
+    std::streambuf* buffer = istream.rdbuf();
+
+    while(true) {
+        int c = buffer->sbumpc();
+        switch (c) {
+        case '\n':
+            return istream;
+        case '\r':
+            if(buffer->sgetc() == '\n')
+                buffer->sbumpc();
+            return istream;
+        case EOF:
+            // Also handle the case when the last line has no line ending
+            if(string.empty()) {
+                istream.setstate(std::ios::eofbit);
+            }
+            return istream;
+        default:
+            string += static_cast<char>(c);
+        }
+    }
+}
+
 BasicFile::BasicFile(const std::string& filename, File::Mode mode)
     : TextFile(filename, mode) {
     std::ios_base::openmode openmode;
@@ -39,7 +75,7 @@ BasicFile::BasicFile(const std::string& filename, File::Mode mode)
 
 std::string BasicFile::readline() {
     std::string line;
-    std::getline(stream_, line);
+    get_line(stream_, line);
     return line;
 }
 
@@ -55,7 +91,7 @@ bool BasicFile::eof() {
 std::vector<std::string> BasicFile::readlines(size_t n) {
     auto lines = std::vector<std::string>(n);
     for (size_t i = 0; i < n; i++) {
-        std::getline(stream_, lines[i]);
+        get_line(stream_, lines[i]);
         if (!stream_) {
             throw FileError("Error while reading file " + filename());
         }
