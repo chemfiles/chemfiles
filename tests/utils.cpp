b703786453a746ea26e14783328d6f003b41cf89
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-05-16 11:34:03 +0200

    More tests for the utils header

--- tests/utils.cpp
+++ tests/utils.cpp
@@ -17,3 +17,23 @@ TEST_CASE("Current directory") {
     auto cwd = fs::current_path();
     CHECK(chemfiles::current_directory() == cwd.string());
 }
+
+TEST_CASE("trim") {
+    CHECK(chemfiles::trim("   left") == "left");
+    CHECK(chemfiles::trim("right    ") == "right");
+    CHECK(chemfiles::trim("   both   \t") == "both");
+    CHECK(chemfiles::trim("tabs\t") == "tabs");
+    CHECK(chemfiles::trim("lines\n") == "lines");
+    CHECK(chemfiles::trim("lines\r") == "lines");
+    CHECK(chemfiles::trim("lines\r\n") == "lines");
+    CHECK(chemfiles::trim("\r\nlines\r") == "lines");
+    CHECK(chemfiles::trim("   and \t with\rsome\n   inside  \t") == "and \t with\rsome\n   inside");
+}
+
+TEST_CASE("split") {
+    auto expected = std::vector<std::string>{"bla", "bla  bla, jk", "fiuks"};
+    CHECK(chemfiles::split("bla:bla  bla, jk:fiuks", ':') == expected);
+
+    expected = std::vector<std::string>{"", "", "bla  bla", " jk:fiuks"};
+    CHECK(chemfiles::split(",,bla  bla, jk:fiuks", ',') == expected);
+}
