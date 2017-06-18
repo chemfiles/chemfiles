c4adcf8e40478f178ea246f5df8672a5c2ecc203
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-06-18 16:31:13 +0200

    Allow to add configuration files from the C API

--- tests/capi/misc.cpp
+++ tests/capi/misc.cpp
@@ -25,23 +25,9 @@ TEST_CASE("Errors", "[CAPI]") {
     CHECK(chfl_last_error() == std::string(""));
 }
 
-// Global variables for access from callback and main
-static char* buffer = NULL;
-
-static void callback(const char* message) {
-    size_t size = strlen(message) + 1;
-    REQUIRE(buffer == NULL);
-    buffer = static_cast<char*>(malloc(sizeof(char) * size));
-    strcpy(buffer, message);
-}
-
-TEST_CASE("Logging", "[CAPI]") {
-    CHECK_STATUS(chfl_set_warning_callback(callback));
-
-    std::string message = generate_chemfiles_error();
-
-    CHECK(buffer == message);
-    free(buffer);
+TEST_CASE("Configuration", "[CAPI]") {
+    CHECK_STATUS(chfl_add_configuration("another_config"));
+    CHECK(chfl_add_configuration("not-there") == CHFL_CONFIGURATION_ERROR);
 }
 
 TEST_CASE("Version", "[CAPI]") {
@@ -54,4 +40,23 @@ TEST_CASE("Version", "[CAPI]") {
     // Remove trailling \n
     std::string version = content.str().substr(0, content.str().length() - 1);
     CHECK(chfl_version() == version);
+}
+
+// Global variables for access from callback and main
+static char* buffer = NULL;
+
+static void callback(const char* message) {
+    size_t size = strlen(message) + 1;
+    REQUIRE(buffer == NULL);
+    buffer = static_cast<char*>(malloc(sizeof(char) * size));
+    strcpy(buffer, message);
+}
+
+TEST_CASE("Warnings", "[CAPI]") {
+    CHECK_STATUS(chfl_set_warning_callback(callback));
+
+    std::string message = generate_chemfiles_error();
+
+    CHECK(buffer == message);
+    free(buffer);
 }
