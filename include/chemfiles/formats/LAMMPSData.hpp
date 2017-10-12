fd46f105c5383d671a9e6b07d233680e4aee144c
Guillaume Fraux <luthaf@luthaf.fr>
2017-10-12 17:10:47 +0200

    Add write capacities to LAMMPS Data format (#111)

--- include/chemfiles/formats/LAMMPSData.hpp
+++ include/chemfiles/formats/LAMMPSData.hpp
@@ -9,6 +9,8 @@
 
 #include "chemfiles/Format.hpp"
 #include "chemfiles/File.hpp"
+#include "chemfiles/Topology.hpp"
+#include "chemfiles/sorted_set.hpp"
 
 namespace chemfiles {
 
@@ -45,6 +47,62 @@ public:
     atom_data read_line(const std::string& line) const;
 };
 
+// atom types are defined by the type string and the mass of the atom
+using atom_type = std::pair<std::string, double>;
+using bond_type = std::tuple<size_t, size_t>;
+using angle_type = std::tuple<size_t, size_t, size_t>;
+using dihedral_type = std::tuple<size_t, size_t, size_t, size_t>;
+
+class DataTypes {
+public:
+    DataTypes(const Topology& topology = Topology());
+
+    DataTypes(const DataTypes&) = delete;
+    DataTypes& operator=(const DataTypes&) = delete;
+    DataTypes(DataTypes&&) = default;
+    DataTypes& operator=(DataTypes&&) = default;
+
+    const sorted_set<atom_type>& atoms() const {return atoms_;}
+    const sorted_set<bond_type>& bonds() const {return bonds_;}
+    const sorted_set<angle_type>& angles() const {return angles_;}
+    const sorted_set<dihedral_type>& dihedrals() const {return dihedrals_;}
+
+    /// Get the atom type number for the given atom.
+    ///
+    /// The atom must be in the topology used to construct this `DataTypes`
+    /// instance. The index numbering starts at zero, and can be used to index
+    /// the vector backing the `sorted_set<atom_type>` returned by `atoms()`.
+    size_t atom_type_id(const Atom& atom) const;
+
+    /// Get the bond type number for the bond type i-j.
+    ///
+    /// The bond type must be in the topology used to construct this `DataTypes`
+    /// instance. The index numbering starts at zero, and can be used to index
+    /// the vector backing the `sorted_set<bond_type>` returned by `bonds()`.
+    size_t bond_type_id(size_t type_i, size_t type_j) const;
+
+    /// Get the angle type number for the angle type i-j-k.
+    ///
+    /// The angle type must be in the topology used to construct this `DataTypes`
+    /// instance. The index numbering starts at zero, and can be used to index
+    /// the vector backing the `sorted_set<angle_type>` returned by `angles()`.
+    size_t angle_type_id(size_t type_i, size_t type_j, size_t type_k) const;
+
+    /// Get the dihedral type number for the angle type i-j-k.
+    ///
+    /// The dihedral type must be in the topology used to construct this
+    /// `DataTypes` instance. The index numbering starts at zero, and can be
+    /// used to index the vector backing the `sorted_set<dihedral_type>`
+    /// returned by `dihedrals()`.
+    size_t dihedral_type_id(size_t type_i, size_t type_j, size_t type_k, size_t type_m) const;
+
+private:
+    sorted_set<atom_type> atoms_;
+    sorted_set<bond_type> bonds_;
+    sorted_set<angle_type> angles_;
+    sorted_set<dihedral_type> dihedrals_;
+};
+
 /// [LAMMPS Data] file format reader and writer.
 ///
 /// LAMMPS data files are not fully stand-alone, as one needs to know the atom
@@ -67,6 +125,7 @@ public:
 
     void read_step(size_t step, Frame& frame) override;
     void read(Frame& frame) override;
+    void write(const Frame& frame) override;
     size_t nsteps() override;
 private:
     enum section_t {
@@ -107,8 +166,27 @@ private:
     /// Setup atomic names with previously read values
     void setup_names(Frame& frame) const;
 
+    /// Write the header
+    void write_header(const Frame& frame);
+    /// Write the types sections
+    void write_types();
+    /// Write the masses section
+    void write_masses();
+    /// Write the Atoms section
+    void write_atoms(const Frame& frame);
+    /// Write the Velocities section
+    void write_velocities(const Frame& frame);
+    /// Write the Bonds section
+    void write_bonds(const Topology& topology);
+    /// Write the Angles section
+    void write_angles(const Topology& topology);
+    /// Write the Dihedrals section
+    void write_dihedrals(const Topology& topology);
+
     /// Text file where we read from
     std::unique_ptr<TextFile> file_;
+
+    // =============== Data used for reading files
     /// Name of the atom style to use when reading the "Atoms" section
     std::string atom_style_name_ = "";
     /// Atom style to use when reading the "Atoms" section
@@ -123,6 +201,11 @@ private:
     std::unordered_map<std::string, double> masses_;
     /// Optional atomic names, indexed by atomic indexes
     std::vector<std::string> names_;
+
+    // =============== Data used for writting files
+    /// Did we already wrote a frame to this file
+    bool written_ = false;
+    DataTypes types_;
 };
 
 } // namespace chemfiles
