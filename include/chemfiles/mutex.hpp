a64044a2c681a8f932df7044ae7f1fa99c839cc2
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-10-26 13:34:06 +0000

    Add a mutex<T> class to guanrantee exclusive access to some data
    This class ensures the mutex is always locked when accessig the data

---
+++ include/chemfiles/mutex.hpp
@@ -0,0 +1,71 @@
+// Chemfiles, a modern library for chemistry file reading and writing
+// Copyright (C) Guillaume Fraux and contributors -- BSD license
+
+#ifndef CHEMFILES_MUTEX_HPP
+#define CHEMFILES_MUTEX_HPP
+
+#include <mutex>
+
+namespace chemfiles {
+
+template<class T> class mutex;
+
+/// A lock guard that guarantee exlusive access to the underlying data.
+///
+/// When the lock guard is destroyed, it releases the associated mutex.
+template<class T>
+class lock_guard {
+public:
+    T& operator*() {
+        return data_;
+    }
+
+    const T& operator*() const {
+        return data_;
+    }
+
+private:
+    lock_guard(T& data, std::unique_lock<std::mutex>&& guard):
+        data_(data), guard_(std::move(guard)) {}
+
+    T& data_;
+    std::unique_lock<std::mutex> guard_;
+
+    friend class mutex<T>;
+};
+
+/// A `std::mutex` wrapper that own some associated data, and ensure that the
+/// mutex is always locked before accessing the data
+template<class T>
+class mutex {
+public:
+    /// Create a new mutex containing the given `data`
+    mutex(T data): data_(std::move(data)), mutex_() {}
+
+    mutex(const mutex&) = delete;
+    mutex& operator=(const mutex&) = delete;
+    mutex(mutex&&) = default;
+    mutex& operator=(mutex&&) = default;
+
+    ~mutex() {
+        // deadlock if someone is trying to destroy this `chemfiles::mutex`
+        // while still holding a lock on the underlying `mutex_`.
+        mutex_.lock();
+        mutex_.unlock();
+    }
+
+    /// Lock the mutex, and return a `lock_guard`. The `lock_guard` allow to
+    /// access the locked data, and will release the mutex when it goes out of
+    /// scope.
+    lock_guard<T> lock() {
+        return lock_guard<T>(data_, std::unique_lock<std::mutex>(mutex_));
+    }
+
+private:
+    T data_;
+    std::mutex mutex_;
+};
+
+} // namespace chemfiles
+
+#endif
