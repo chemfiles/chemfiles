ad4a3fb73305db06f436a2ef7925943da9a5e1e9
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-10-26 12:24:14 +0000

    Ensure thread safety for warning callback code

--- src/generic.cpp
+++ src/generic.cpp
@@ -2,20 +2,26 @@
 // Copyright (C) Guillaume Fraux and contributors -- BSD license
 
 #include <iostream>
+
 #include "chemfiles/warnings.hpp"
 #include "chemfiles/Configuration.hpp"
 #include "chemfiles/generic.hpp"
+#include "chemfiles/mutex.hpp"
 
-static chemfiles::warning_callback CALLBACK = [](std::string message){
+using namespace chemfiles;
+
+static mutex<warning_callback> CALLBACK = {[](std::string message){
     std::cerr << "[chemfiles] " << message << std::endl;
-};
+}};
 
 void chemfiles::set_warning_callback(warning_callback callback) {
-    CALLBACK = callback;
+    auto guard = CALLBACK.lock();
+    *guard = callback;
 }
 
 void chemfiles::warning(std::string message) {
-    CALLBACK(std::move(message));
+    auto guard = CALLBACK.lock();
+    (*guard)(std::move(message));
 }
 
 void chemfiles::add_configuration(const std::string& path) {
