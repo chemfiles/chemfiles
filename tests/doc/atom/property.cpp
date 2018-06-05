9cf52ed15bc7a6b6ea0b12ad58db413678ea4614
pbarletta <pbarletta@gmail.com>
2018-06-05 16:25:09 -0300

    Added C++ Properties iteration example.

--- tests/doc/atom/property.cpp
+++ tests/doc/atom/property.cpp
@@ -22,5 +22,26 @@ TEST_CASE() {
     // Override the "first" property
     atom.set("first", false);
     assert(atom.get("first")->as_bool() == false);
+
+    // Iterate over all properties
+    atom.set("third", "string");
+    atom.set("fourth", Vector3D(1., 2., 3.));
+    for(auto prop_ite = atom.properties_begin(); prop_ite != atom.properties_end() ; ++prop_ite) {
+        auto kind = prop_ite->second.get_kind();
+        switch(kind) {
+            case Property::BOOL:
+                assert(prop_ite->second.as_bool() == false);
+                break;
+            case Property::DOUBLE:
+                assert(prop_ite->second.as_double() == 42.5);
+                break;
+            case Property::STRING:
+                assert(prop_ite->second.as_string() == "string");
+                break;
+            case Property::VECTOR3D:
+                assert(prop_ite->second.as_vector3d() == Vector3D(1., 2., 3.));
+                break;
+        }
+    }
     // [example]
 }
