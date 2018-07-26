51e2cf66b98a10a37670a6927800f9b65170e4e8
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2018-07-26 10:32:17 +0000

    Disable debuginfo for faster link time on MinGW

--- scripts/ci/setup-appveyor.ps1
+++ scripts/ci/setup-appveyor.ps1
@@ -1,5 +1,5 @@
 if ("$env:CXX_PATH" -ne "") {
-    $env:PATH += ";$env:CXX_PATH"
+    $env:PATH = "$env:CXX_PATH;$env:PATH"
 }
 
 $env:CMAKE_ARGUMENTS = "-G `"$env:generator`""
@@ -10,11 +10,13 @@ $env:CMAKE_ARGUMENTS += " -DCHFL_BUILD_DOCTESTS=OFF"
 
 if ($env:generator -Match "Visual Studio") {
     $env:BUILD_ARGUMENTS="/verbosity:minimal /m:2"
-} else {
+} elseif ($env:generator -eq "MinGW Makefiles") {
     $env:BUILD_ARGUMENTS="-j2"
-}
 
-if ($env:generator -eq "MinGW Makefiles") {
+    # Disable debug information for faster link time
+    $env:CMAKE_ARGUMENTS += " -DCMAKE_CXX_FLAGS_DEBUG=''"
+    $env:CMAKE_ARGUMENTS += " -DCMAKE_C_FLAGS_DEBUG=''"
+
     # Remove sh.exe from git in the PATH for MinGW to work
     $env:PATH = ($env:PATH.Split(';') | Where-Object { $_ -ne 'C:\Program Files\Git\usr\bin' }) -join ';'
 }
