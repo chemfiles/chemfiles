44a4f7ebed5ec5954eb4dd05121644f6caf5f1aa
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2018-09-16 20:41:06 +0200

    Start documenting all properties used by chemfiles

--- doc/ext/chfl_example.py
+++ doc/ext/chfl_example.py
@@ -1,17 +1,16 @@
 from sphinx.directives.code import LiteralInclude
 
-class Example(LiteralInclude):
-    """
-    TODO
-    """
+
+class CHFLExample(LiteralInclude):
+    """Add a code example for chemfiles documentation"""
 
     def run(self):
         self.options["language"] = "cpp"
         self.options["start-after"] = "[example]"
         self.options["end-before"] = "[example]"
         self.options["dedent"] = 4
-        return super(Example, self).run()
+        return super(CHFLExample, self).run()
 
 
 def setup(app):
-    app.add_directive("chfl_example", Example)
+    app.add_directive("chfl_example", CHFLExample)
