c9937210e542cb3c0d2a1e8edf324f5c558630d9
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2019-03-14 11:25:43 +0100

    Remove more not interesting warnings from MSVC

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -113,18 +113,26 @@ if(MSVC)
     remove_msvc_warning(4251) # class <> needs to be dll-export
     remove_msvc_warning(4503) # mangled name too long was truncated
     remove_msvc_warning(4514) # unreferenced inline function has been removed
+    remove_msvc_warning(4571) # catch(...) semantics changed since Visual C++ 7.1;
     remove_msvc_warning(4582) # constructor is not implicitly called
     remove_msvc_warning(4583) # destructor is not implicitly called
     remove_msvc_warning(4623) # default constructor was implicitly defined as deleted
     remove_msvc_warning(4625) # copy constructor was implicitly defined as deleted
     remove_msvc_warning(4626) # assignment operator was implicitly defined as deleted
+    remove_msvc_warning(4643) # forward declaring 'optional' in namespace std is not permitted by the C++ Standard
     remove_msvc_warning(4668) # not defined preprocessor macro, replacing with '0' for '#if/#elif'
     remove_msvc_warning(4627) # move assignment operator was implicitly defined as deleted
     remove_msvc_warning(4710) # function not inlined
     remove_msvc_warning(4711) # function selected for automatic inlining
     remove_msvc_warning(4820) # padding added
+    remove_msvc_warning(4868) # compiler may not enforce left-to-right evaluation order in braced initializer list
     remove_msvc_warning(5026) # move constructor was implicitly defined as deleted
     remove_msvc_warning(5027) # move assignment operator was implicitly defined as deleted
+    remove_msvc_warning(5039) # pointer or reference to potentially throwing function passed to extern C function under -EHc.
+
+    # Noisy warnings from external code that are checked by clang
+    remove_msvc_warning(4365) # conversion from '...' to '...', signed/unsigned mismatch
+    remove_msvc_warning(4774) # format string is not a string literal
 else()
     # Add some warnings in debug mode
     # Basic set of warnings
