ba479f7a8dcbfd27394fb9b1756ea4d31994c468
Jonathan Fine <finej@purdue.edu>
2019-05-08 13:34:46 -0400

    Remove atom in residue when removed from topology

--- src/Residue.cpp
+++ src/Residue.cpp
@@ -16,3 +16,16 @@ void Residue::add_atom(size_t i) {
 bool Residue::contains(size_t i) const {
     return atoms_.find(i) != atoms_.end();
 }
+
+void Residue::atom_removed(size_t i) {
+    auto iter = atoms_.find(i);
+    if (iter != atoms_.end()) {
+        atoms_.erase(iter);
+    }
+
+    for (size_t j = 0; j < atoms_.size(); ++j) {
+        if (atoms_[j] > i) {
+            --atoms_[j];
+        }
+    }
+}
