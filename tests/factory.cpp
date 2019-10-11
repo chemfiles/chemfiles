de68aa6cc74b55ea3020dbbd2d25232937eb2be3
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2019-10-12 00:37:32 +0200

    Improve code coverage

--- tests/factory.cpp
+++ tests/factory.cpp
@@ -14,10 +14,11 @@
 using namespace chemfiles;
 
 #define new_format(_name_)                                                     \
-    struct _name_: public Format {                                             \
+    struct _name_ final: public Format {                                       \
         _name_(const std::string&, File::Mode, File::Compression) {}           \
         size_t nsteps() override {return 42;}                                  \
     }
+
 
 new_format(DummyFormat);
 new_format(DunnyFormat);
@@ -26,6 +27,21 @@ new_format(BadExtensionFormat);
 new_format(NoFormatInfo);
 new_format(SameNameFormat);
 new_format(SameExtensionFormat);
+
+struct UnimplementedTextFormat final: public TextFormat {
+    UnimplementedTextFormat(const std::string& path, File::Mode mode, File::Compression compression):
+        TextFormat(std::move(path), mode, compression) {}
+
+    int64_t forward() override {
+        static int64_t pos = -1;
+        if (pos < 4) {
+            pos++;
+            return pos;
+        } else {
+            return -1;
+        }
+    }
+};
 
 namespace chemfiles {
     template<> FormatInfo format_information<DummyFormat>() {
@@ -51,6 +67,10 @@ namespace chemfiles {
     template<> FormatInfo format_information<BadExtensionFormat>() {
         return FormatInfo("BadExtension").with_extension("no-dot");
     }
+
+    template<> FormatInfo format_information<UnimplementedTextFormat>() {
+        return FormatInfo("UnimplementedTextFormat").with_extension(".UnimplementedTextFormat");
+    }
 }
 
 TEST_CASE("Geting registered format") {
@@ -62,13 +82,25 @@ TEST_CASE("Geting registered format") {
     format = FormatFactory::get().name("Dummy")("", File::READ, File::DEFAULT);
     CHECK(typeid(dummy) == typeid(*format));
 
-    CHECK_THROWS_AS(FormatFactory::get().name("UNKOWN"), FormatError);
-    CHECK_THROWS_AS(FormatFactory::get().extension(".UNKOWN"), FormatError);
+    CHECK_THROWS_WITH(
+        FormatFactory::get().name("UNKOWN"),
+        "can not find a format named 'UNKOWN'"
+    );
+    CHECK_THROWS_WITH(
+        FormatFactory::get().extension(".UNKOWN"),
+        "can not find a format associated with the '.UNKOWN' extension"
+    );
 }
 
 TEST_CASE("Already registered format/extension") {
-    CHECK_THROWS_AS(FormatFactory::get().add_format<SameNameFormat>(), FormatError);
-    CHECK_THROWS_AS(FormatFactory::get().add_format<SameExtensionFormat>(), FormatError);
+    CHECK_THROWS_WITH(
+        FormatFactory::get().add_format<SameNameFormat>(),
+        "there is already a format associated with the name 'Dummy'"
+    );
+    CHECK_THROWS_WITH(
+        FormatFactory::get().add_format<SameExtensionFormat>(),
+        "the extension '.dummy' is already associated with format 'Dummy'"
+    );
 }
 
 TEST_CASE("Format names suggestions") {
@@ -98,21 +130,71 @@ TEST_CASE("Format names suggestions") {
 }
 
 TEST_CASE("Bad format info") {
-    CHECK_THROWS_AS(format_information<NoNameFormat>(), FormatError);
-    CHECK_THROWS_AS(format_information<BadExtensionFormat>(), FormatError);
-    CHECK_THROWS_AS(format_information<NoFormatInfo>(), FormatError);
+    CHECK_THROWS_WITH(
+        format_information<NoNameFormat>(),
+        "a format name can not be an empty string"
+    );
+
+    CHECK_THROWS_WITH(
+        format_information<BadExtensionFormat>(),
+        "a format extension must start with a dot"
+    );
+
+    CHECK_THROWS_WITH(
+        format_information<NoFormatInfo>(),
+        "format_informations is unimplemented for this format"
+    );
 }
 
 TEST_CASE("Check error throwing in formats") {
-    auto tmpfile = NamedTempPath(".dummy");
-    std::ofstream out(tmpfile);
-    out << "hey !" << std::endl;
-    out.close();
+    SECTION("Format") {
+        auto tmpfile = NamedTempPath(".dummy");
+        std::ofstream out(tmpfile);
+        out << "hey !" << std::endl;
+        out.close();
 
-    Frame frame;
-    Trajectory trajectory_r(tmpfile, 'r');
-    CHECK_THROWS_AS(trajectory_r.read(), FormatError);
-    CHECK_THROWS_AS(trajectory_r.read_step(2), FormatError);
-    Trajectory trajectory_w(tmpfile, 'w');
-    CHECK_THROWS_AS(trajectory_w.write(frame), FormatError);
+        auto trajectory = Trajectory(tmpfile, 'r');
+        CHECK_THROWS_WITH(
+            trajectory.read(),
+            Catch::StartsWith("'read' is not implemented for this format")
+        );
+        CHECK_THROWS_WITH(
+            trajectory.read_step(2),
+            Catch::StartsWith("'read_step' is not implemented for this format")
+        );
+
+        auto frame = Frame();
+        trajectory = Trajectory (tmpfile, 'w');
+        CHECK_THROWS_WITH(
+            trajectory.write(frame),
+            Catch::StartsWith("'write' is not implemented for this format")
+        );
+    }
+
+    SECTION("Text Format") {
+        FormatFactory::get().add_format<UnimplementedTextFormat>();
+
+        auto tmpfile = NamedTempPath(".UnimplementedTextFormat");
+        std::ofstream out(tmpfile);
+        out << "hey !" << std::endl;
+        out.close();
+
+        auto trajectory = Trajectory(tmpfile, 'r');
+        CHECK_THROWS_WITH(
+            trajectory.read(),
+            Catch::StartsWith("'read' is not implemented for this format")
+        );
+        CHECK_THROWS_WITH(
+            trajectory.read_step(2),
+            Catch::StartsWith("'read' is not implemented for this format")
+        );
+
+        auto frame = Frame();
+        trajectory = Trajectory (tmpfile, 'w');
+        CHECK_THROWS_WITH(
+            trajectory.write(frame),
+            Catch::StartsWith("'write' is not implemented for this format")
+        );
+    }
+
 }
