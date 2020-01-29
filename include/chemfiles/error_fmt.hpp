b5a4685a95376a53503afe5a971554a4c5bbdb0c
Guillaume Fraux <guillaume.fraux@epfl.ch>
2020-01-29 16:36:26 +0100

    Use perfect forwarding in error_fmt

--- include/chemfiles/error_fmt.hpp
+++ include/chemfiles/error_fmt.hpp
@@ -17,50 +17,50 @@ namespace chemfiles {
 
 /// Create an `Error` using the given `format` and `arguments`.
 template <typename... Args>
-inline Error error(const char *format, const Args & ... arguments) {
-    return Error(fmt::format(format, arguments...));
+inline Error error(const char *format, Args && ... arguments) {
+    return Error(fmt::format(format, std::forward<Args>(arguments)...));
 }
 
 /// Create a `FileError` using the given `format` and `arguments`.
 template <typename... Args>
-inline FileError file_error(const char *format, const Args & ... arguments) {
-    return FileError(fmt::format(format, arguments...));
+inline FileError file_error(const char *format, Args && ... arguments) {
+    return FileError(fmt::format(format, std::forward<Args>(arguments)...));
 }
 
 /// Create a `MemoryError` using the given `format` and `arguments`.
 template <typename... Args>
-inline MemoryError memory_error(const char *format, const Args & ... arguments) {
-    return MemoryError(fmt::format(format, arguments...));
+inline MemoryError memory_error(const char *format, Args && ... arguments) {
+    return MemoryError(fmt::format(format, std::forward<Args>(arguments)...));
 }
 
 /// Create a `FormatError` using the given `format` and `arguments`.
 template <typename... Args>
-inline FormatError format_error(const char *format, const Args & ... arguments) {
-    return FormatError(fmt::format(format, arguments...));
+inline FormatError format_error(const char *format, Args && ... arguments) {
+    return FormatError(fmt::format(format, std::forward<Args>(arguments)...));
 }
 
 /// Create a `SelectionError` using the given `format` and `arguments`.
 template <typename... Args>
-inline SelectionError selection_error(const char *format, const Args & ... arguments) {
-    return SelectionError(fmt::format(format, arguments...));
+inline SelectionError selection_error(const char *format, Args && ... arguments) {
+    return SelectionError(fmt::format(format, std::forward<Args>(arguments)...));
 }
 
 /// Create a `ConfigurationError` using the given `format` and `arguments`.
 template <typename... Args>
-inline ConfigurationError configuration_error(const char *format, const Args & ... arguments) {
-    return ConfigurationError(fmt::format(format, arguments...));
+inline ConfigurationError configuration_error(const char *format, Args && ... arguments) {
+    return ConfigurationError(fmt::format(format, std::forward<Args>(arguments)...));
 }
 
 /// Create an `OutOfBounds` error using the given `format` and `arguments`.
 template <typename... Args>
-inline OutOfBounds out_of_bounds(const char *format, const Args & ... arguments) {
-    return OutOfBounds(fmt::format(format, arguments...));
+inline OutOfBounds out_of_bounds(const char *format, Args && ... arguments) {
+    return OutOfBounds(fmt::format(format, std::forward<Args>(arguments)...));
 }
 
 /// Create a `PropertyError` using the given `format` and `arguments`.
 template <typename... Args>
-inline PropertyError property_error(const char *format, const Args & ... arguments) {
-    return PropertyError(fmt::format(format, arguments...));
+inline PropertyError property_error(const char *format, Args && ... arguments) {
+    return PropertyError(fmt::format(format, std::forward<Args>(arguments)...));
 }
 
 } // namespace chemfiles
