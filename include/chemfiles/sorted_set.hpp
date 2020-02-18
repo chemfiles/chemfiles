0b815234794dbfe09c2e8682b47b405910bbf6a5
Guillaume Fraux <guillaume.fraux@epfl.ch>
2020-02-18 14:22:38 +0100

    Disable mutable indexing in sorted_vec

--- include/chemfiles/sorted_set.hpp
+++ include/chemfiles/sorted_set.hpp
@@ -26,7 +26,6 @@ public:
     sorted_set(sorted_set&&) = default;
     sorted_set& operator=(sorted_set&&) = default;
 
-    using super::operator[];
     using super::cbegin;
     using super::cend;
     using super::crbegin;
@@ -35,6 +34,8 @@ public:
     const_iterator end() const {return super::end();}
     const_iterator rbegin() const {return super::rbegin();}
     const_iterator rend() const {return super::rend();}
+
+    const T& operator[](size_t i) const {return super::operator[](i);}
 
     using super::empty;
     using super::size;
@@ -88,6 +89,14 @@ public:
 
     /// Get the underlying vector data wihtout a copy
     const super& as_vec() const {
+        return *this;
+    }
+
+    /// Get the underlying vector data wihtout a copy.
+    ///
+    /// WARNING: the vector is not const and can be modified, the user is
+    /// supposed to ensure that the values inside stay ordered.
+    super& as_mutable_vec() {
         return *this;
     }
 };
