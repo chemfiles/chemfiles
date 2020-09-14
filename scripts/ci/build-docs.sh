7468e6112636485720a7b1340d7b8977c1effcff
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2020-09-14 14:42:33 +0200

    Update documentation requirements
    In particular, require a more recent doxygen version to ensure ^^
    is interpreted as a newline in aliases

--- scripts/ci/build-docs.sh
+++ scripts/ci/build-docs.sh
@@ -6,8 +6,8 @@ set -xe
 cd $TRAVIS_BUILD_DIR
 python3.6 -m pip install --user --upgrade -r doc/requirements.txt
 
-DOXYGEN_VER=1.8.13_1
-DOXYGEN_URL="https://linuxbrew.bintray.com/bottles/doxygen-${DOXYGEN_VER}.x86_64_linux.bottle.1.tar.gz"
+DOXYGEN_VER=1.8.20
+DOXYGEN_URL="https://linuxbrew.bintray.com/bottles/doxygen-${DOXYGEN_VER}.x86_64_linux.bottle.tar.gz"
 wget -O - "${DOXYGEN_URL}" | tar xz -C /tmp doxygen/${DOXYGEN_VER}/bin/doxygen
 export PATH="/tmp/doxygen/${DOXYGEN_VER}/bin:$PATH"
 
