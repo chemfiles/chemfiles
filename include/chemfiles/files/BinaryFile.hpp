787f3eed47b631eb34543792bcb45c4835140ce1
ezavod <ezavod@gmx.de>
2022-05-30 10:43:58 +0200

    refactor(BinaryFile): overload read/write functions for vector
    Overload read/write functions to take pre-allocated vectors of a given
    type. These functions are implemented for all types used by the binary
    file.

--- include/chemfiles/files/BinaryFile.hpp
+++ include/chemfiles/files/BinaryFile.hpp
@@ -60,7 +60,8 @@ public:
 
     /// Read exactly `count` char, and store them in the `data` array
     void read_char(char* data, size_t count);
-
+    /// Read exactly as many char as fit in the pre-allocated vector
+    void read_char(std::vector<char>& data) { this->read_char(data.data(), data.size()); }
     /// Read a single char value from the file
     char read_single_char() {
         char value;
@@ -73,6 +74,8 @@ public:
     void read_i8(int8_t* data, size_t count) {
         this->read_char(reinterpret_cast<char*>(data), count);
     }
+    /// Read exactly as many 8-bit signed integers as fit in the pre-allocated vector
+    void read_i8(std::vector<int8_t>& data) { this->read_i8(data.data(), data.size()); }
     /// Read a single 8-bit signed integer from the file
     int8_t read_single_i8() {
         int8_t value;
@@ -85,6 +88,8 @@ public:
     void read_u8(uint8_t* data, size_t count) {
         this->read_char(reinterpret_cast<char*>(data), count);
     }
+    /// Read exactly as many 8-bit unsigned integers as fit in the pre-allocated vector
+    void read_u8(std::vector<uint8_t>& data) { this->read_u8(data.data(), data.size()); }
     /// Read a single 8-bit unsigned integer from the file
     uint8_t read_single_u8() {
         uint8_t value;
@@ -95,6 +100,8 @@ public:
     /// Read exactly `count` 16-bit signed integers, and store them in the
     /// `data` array
     virtual void read_i16(int16_t* data, size_t count) = 0;
+    /// Read exactly as many 16-bit signed integers as fit in the pre-allocated vector
+    void read_i16(std::vector<int16_t>& data) { this->read_i16(data.data(), data.size()); }
     /// Read a single 16-bit signed integer from the file
     int16_t read_single_i16() {
         int16_t value;
@@ -105,6 +112,8 @@ public:
     /// Read exactly `count` 16-bit unsigned integers, and store them in the
     /// `data` array
     virtual void read_u16(uint16_t* data, size_t count) = 0;
+    /// Read exactly as many 16-bit unsigned integers as fit in the pre-allocated vector
+    void read_u16(std::vector<uint16_t>& data) { this->read_u16(data.data(), data.size()); }
     /// Read a single 16-bit unsigned integer from the file
     uint16_t read_single_u16() {
         uint16_t value;
@@ -115,6 +124,8 @@ public:
     /// Read exactly `count` 32-bit signed integers, and store them in the
     /// `data` array
     virtual void read_i32(int32_t* data, size_t count) = 0;
+    /// Read exactly as many 32-bit signed integers as fit in the pre-allocated vector
+    void read_i32(std::vector<int32_t>& data) { this->read_i32(data.data(), data.size()); }
     /// Read a single 32-bit signed integer from the file
     int32_t read_single_i32() {
         int32_t value;
@@ -125,6 +136,8 @@ public:
     /// Read exactly `count` 32-bit unsigned integers, and store them in the
     /// `data` array
     virtual void read_u32(uint32_t* data, size_t count) = 0;
+    /// Read exactly as many 32-bit unsigned integers as fit in the pre-allocated vector
+    void read_u32(std::vector<uint32_t>& data) { this->read_u32(data.data(), data.size()); }
     /// Read a single 32-bit unsigned integer from the file
     uint32_t read_single_u32() {
         uint32_t value;
@@ -135,6 +148,8 @@ public:
     /// Read exactly `count` 64-bit signed integers, and store them in the
     /// `data` array
     virtual void read_i64(int64_t* data, size_t count) = 0;
+    /// Read exactly as many 64-bit signed integers as fit in the pre-allocated vector
+    void read_i64(std::vector<int64_t>& data) { this->read_i64(data.data(), data.size()); }
     /// Read a single 64-bit signed integer from the file
     int64_t read_single_i64() {
         int64_t value;
@@ -145,6 +160,8 @@ public:
     /// Read exactly `count` 64-bit unsigned integers, and store them in the
     /// `data` array
     virtual void read_u64(uint64_t* data, size_t count) = 0;
+    /// Read exactly as many 64-bit unsigned integers as fit in the pre-allocated vector
+    void read_u64(std::vector<uint64_t>& data) { this->read_u64(data.data(), data.size()); }
     /// Read a single 64-bit unsigned integer from the file
     uint64_t read_single_u64() {
         uint64_t value;
@@ -155,32 +172,36 @@ public:
     /// Read exactly `count` 32-bit floating point numbers, and store them in
     /// the `data` array
     virtual void read_f32(float* data, size_t count) = 0;
+    /// Read exactly as many 32-bit floating point numbers as fit in the pre-allocated vector
+    void read_f32(std::vector<float>& data) { this->read_f32(data.data(), data.size()); }
     /// Read a single 32-bit floating point number from the file
     float read_single_f32() {
         float value;
         this->read_f32(&value, 1);
         return value;
     }
-    /// Read exactly as many 32-bit floating point numbers as fit in the pre-allocated vector
-    void read_f32_array(std::vector<float>& data) { this->read_f32(data.data(), data.size()); }
 
     /// Read exactly `count` 64-bit floating point numbers, and store them in
     /// the `data` array
     virtual void read_f64(double* data, size_t count) = 0;
+    /// Read exactly as many 64-bit floating point numbers as fit in the pre-allocated vector
+    void read_f64(std::vector<double>& data) { this->read_f64(data.data(), data.size()); }
     /// Read a single 64-bit floating point number from the file
     double read_single_f64() {
         double value;
         this->read_f64(&value, 1);
         return value;
     }
-    /// Read exactly as many 64-bit floating point numbers as fit in the pre-allocated vector
-    void read_f64_array(std::vector<double>& data) { this->read_f64(data.data(), data.size()); }
 
 
 
     /// Write exactly `count` char values taken from the `data` array to the
     /// file
     void write_char(const char* data, size_t count);
+    /// Write all char from the pre-allocated vector
+    void write_char(const std::vector<char> data) {
+        this->write_char(data.data(), data.size());
+    }
     /// Write a single char value to the file
     void write_single_char(char value) {
         this->write_char(&value, 1);
@@ -190,6 +211,10 @@ public:
     /// to the file
     void write_i8(const int8_t* data, size_t count) {
         this->write_char(reinterpret_cast<const char*>(data), count);
+    }
+    /// Write all 8-bit signed integers from the pre-allocated vector
+    void write_i8(const std::vector<int8_t> data) {
+        this->write_i8(data.data(), data.size());
     }
     /// Write a single 8-bit signed integer to the file
     void write_single_i8(int8_t value) {
@@ -201,6 +226,10 @@ public:
     void write_u8(const uint8_t* data, size_t count) {
         this->write_char(reinterpret_cast<const char*>(data), count);
     }
+    /// Write all 8-bit unsigned integers from the pre-allocated vector
+    void write_u8(const std::vector<uint8_t> data) {
+        this->write_u8(data.data(), data.size());
+    }
     /// Write a single 8-bit unsigned integer to the file
     void write_single_u8(uint8_t value) {
         this->write_u8(&value, 1);
@@ -209,6 +238,10 @@ public:
     /// Write exactly `count` 16-bit signed integers taken from the `data`
     /// array to the file
     virtual void write_i16(const int16_t* data, size_t count) = 0;
+    /// Write all 16-bit signed integers from the pre-allocated vector
+    void write_i16(const std::vector<int16_t> data) {
+        this->write_i16(data.data(), data.size());
+    }
     /// Write a single 16-bit signed integer to the file
     void write_single_i16(int16_t value) {
         this->write_i16(&value, 1);
@@ -217,6 +250,10 @@ public:
     /// Write exactly `count` 16-bit unsigned integers taken from the `data`
     /// array to the file
     virtual void write_u16(const uint16_t* data, size_t count) = 0;
+    /// Write all 16-bit unsigned integers from the pre-allocated vector
+    void write_u16(const std::vector<uint16_t> data) {
+        this->write_u16(data.data(), data.size());
+    }
     /// Write a single 16-bit unsigned integer to the file
     void write_single_u16(uint16_t value) {
         this->write_u16(&value, 1);
@@ -225,6 +262,10 @@ public:
     /// Write exactly `count` 32-bit signed integers taken from the `data`
     /// array to the file
     virtual void write_i32(const int32_t* data, size_t count) = 0;
+    /// Write all 32-bit signed integers from the pre-allocated vector
+    void write_i32(const std::vector<int32_t> data) {
+        this->write_i32(data.data(), data.size());
+    }
     /// Write a single 32-bit signed integer to the file
     void write_single_i32(int32_t value) {
         this->write_i32(&value, 1);
@@ -233,6 +274,10 @@ public:
     /// Write exactly `count` 32-bit unsigned integers taken from the `data`
     /// array to the file
     virtual void write_u32(const uint32_t* data, size_t count) = 0;
+    /// Write all 32-bit unsigned integers from the pre-allocated vector
+    void write_u32(const std::vector<uint32_t> data) {
+        this->write_u32(data.data(), data.size());
+    }
     /// Write a single 32-bit unsigned integer to the file
     void write_single_u32(uint32_t value) {
         this->write_u32(&value, 1);
@@ -241,6 +286,10 @@ public:
     /// Write exactly `count` 64-bit signed integers taken from the `data`
     /// array to the file
     virtual void write_i64(const int64_t* data, size_t count) = 0;
+    /// Write all 64-bit signed integers from the pre-allocated vector
+    void write_i64(const std::vector<int64_t> data) {
+        this->write_i64(data.data(), data.size());
+    }
     /// Write a single 64-bit signed integer to the file
     void write_single_i64(int64_t value) {
         this->write_i64(&value, 1);
@@ -249,6 +298,10 @@ public:
     /// Write exactly `count` 64-bit unsigned integers taken from the `data`
     /// array to the file
     virtual void write_u64(const uint64_t* data, size_t count) = 0;
+    /// Write all 64-bit unsigned integers from the pre-allocated vector
+    void write_u64(const std::vector<uint64_t> data) {
+        this->write_u64(data.data(), data.size());
+    }
     /// Write a single 64-bit unsigned integer to the file
     void write_single_u64(uint64_t value) {
         this->write_u64(&value, 1);
@@ -257,25 +310,25 @@ public:
     /// Write exactly `count` 32-bit floating point numbers taken from the
     /// `data` array to the file
     virtual void write_f32(const float* data, size_t count) = 0;
+    /// Write all 32-bit floating point numbers from the pre-allocated vector
+    void write_f32(const std::vector<float> data) {
+        this->write_f32(data.data(), data.size());
+    }
     /// Write a single 32-bit floating point number to the file
     void write_single_f32(float value) {
         this->write_f32(&value, 1);
-    }
-    /// Write all 32-bit floating point numbers from the pre-allocated vector
-    void write_f32_array(const std::vector<float> data) {
-        this->write_f32(data.data(), data.size());
     }
 
     /// Write exactly `count` 64-bit floating point numbers taken from the
     /// `data` array to the file
     virtual void write_f64(const double* data, size_t count) = 0;
+    /// Write all 64-bit floating point numbers from the pre-allocated vector
+    void write_f64(const std::vector<double> data) {
+        this->write_f64(data.data(), data.size());
+    }
     /// Write a single 64-bit floating point number to the file
     void write_single_f64(double value) {
         this->write_f64(&value, 1);
-    }
-    /// Write all 64-bit floating point numbers from the pre-allocated vector
-    void write_f64_array(const std::vector<double> data) {
-        this->write_f64(data.data(), data.size());
     }
 protected:
     std::vector<char> swap_buf_;
